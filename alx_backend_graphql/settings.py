223105384bddeecfceb93ca4224205f63984dcfa
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-08-16 21:36:42 +0000

    testing all changed settings in dir

--- alx_backend_graphql/settings.py
+++ alx_backend_graphql/settings.py
@@ -131,6 +131,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 GRAPHENE = {
-    "SCHEMA": "alx_backend_graphql_crm.schema.schema",
+    "SCHEMA": "alx_backend_graphql.schema.schema",
     "ATOMIC_MUTATIONS": True,
 }
