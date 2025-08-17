517346ef9b4bf0464ef207ab4c40b727ea2daf11
Cephas Yaovi Tay <cephas.tay137@gmail.com>
2025-08-17 22:20:38 +0000

    Rewrote schema in  to include hello

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,8 +1,8 @@
 import graphene
 from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(CRMQuery, graphene.ObjectType):
-    pass
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello, GraphQl!")
 
 class Mutation(CRMMutation, graphene.ObjectType):
     pass
